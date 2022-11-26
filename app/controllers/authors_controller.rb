fd9c178d34a792a4145b00c9dedad564f62dcd8a
Ndiema-S <you@example.com>
2022-11-26 21:19:52 +0300

    complete assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,36 @@
 class AuthorsController < ApplicationController
+
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    
+
+    def show
+      author = Author.find(params[:id])
   
-  def show
-    author = Author.find(params[:id])
-
-    render json: author
-  end
-
-  def create
-    author = Author.create(author_params)
-
-    render json: author, status: :created
-  end
-
-  private
+      render json: author
+    end
   
-  def author_params
-    params.permit(:email, :name)
+
+    def create
+      author = Author.create!(author_params)
+      render json: author, status: :created
+    end
+  
+
+    private
+    
+    def author_params
+      params.permit(:email, :name)
+    end
+  
+
+    def render_not_found_response
+      render json: { error: "Author not found" }, status: :not_found
+    end
+    
+    
+    def render_unprocessable_entity(exception)
+      render json: {errors:  exception.record.errors}, status: :unprocessable_entity
+    end
   end
   
-end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+    validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
